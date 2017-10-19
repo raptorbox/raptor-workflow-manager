614926fd4f3ef446fe21de1abf9935b8ef96fbb7
lcapra <lcapra@fbk.eu>
2017-10-19 09:34:08 +0200

    initial setup

--- node-red/user-auth.js
+++ node-red/user-auth.js
@@ -1,47 +1,46 @@
-var Promise = require("bluebird");
+
+const Promise = require("bluebird");
+const Raptor = require("raptor-sdk")
+
+const config = require("./config")
+
+const raptor = new Raptor(config)
+
+
+
 module.exports = {
    type: "credentials",
    users: function(username) {
-       console.warn(username);
-       return Promise.resolve({ username: "admin", permissions: "*" })
-    //    return when.promise(function(resolve) {
-    //        // Do whatever work is needed to check username is a valid
-    //        // user.
-    //        if (valid) {
-    //            // Resolve with the user object. It must contain
-    //            // properties 'username' and 'permissions'
-    //            var user = { username: "admin", permissions: "*" };
-    //            resolve(user);
-    //        } else {
-    //            // Resolve with null to indicate this user does not exist
-    //            resolve(null);
-    //        }
-    //    });
+       return raptor.Auth().login()
+       .then(()=> {
+           return Promise.resolve()
+       })
+       .catch((e) => {
+           console.log("Err", e)
+           return Promise.resolve(null)
+       })
    },
    authenticate: function(username,password) {
-       console.warn(username, password);
-       return Promise.resolve({ username: "admin", permissions: "*" })
-    //    return when.promise(function(resolve) {
-    //        // Do whatever work is needed to validate the username/password
-    //        // combination.
-    //        if (valid) {
-    //            // Resolve with the user object. Equivalent to having
-    //            // called users(username);
-    //            var user = { username: "admin", permissions: "*" };
-    //            resolve(user);
-    //        } else {
-    //            // Resolve with null to indicate the username/password pair
-    //            // were not valid.
-    //            resolve(null);
-    //        }
-    //    });
+       return raptor.Auth().login().then(()=> {
+           const r = new Raptor({
+               username, password,
+               url: raptor.getConfig().url
+           })
+           return r.Auth().login()
+            .then((user) => {
+                console.warn(user);
+                return Promise.resolve({
+                    username,
+                    permissions: '*'
+                })
+            })
+            .catch((e) => {
+                console.log("Err", e)
+                return Promise.resolve(null)
+            })
+       })
    },
    default: function() {
        return Promise.resolve({anonymous: true, permissions:""})
-    //    return when.promise(function(resolve) {
-    //        // Resolve with the user object for the default user.
-    //        // If no default user exists, resolve with null.
-    //        resolve({anonymous: true, permissions:"read"});
-    //    });
    }
 }
